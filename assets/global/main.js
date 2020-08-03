fcdf36fb6806d138d0b841e3eef83cf043f24eed
AllHailTheSheep <21fasickb@bubblers.us>
2020-08-03 11:45:39 -0700

    clean up sidebar animation and mobile rules

--- assets/global/main.js
+++ assets/global/main.js
@@ -14,5 +14,6 @@ function closeSidebar() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.getElementById("menubtn").classList.remove("hidden");
+    document.getElementById("menubtn").className = "menubtn";
 }
 /* exported closeSidebar */
